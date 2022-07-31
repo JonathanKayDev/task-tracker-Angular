40ea3c8bda34a16892d76ddb6f0c0941b7ffcb1d
JonathanKayDev <67706563+JonathanKay86@users.noreply.github.com>
2022-07-31 15:45:00 +0100

    Add deleteTask function

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -16,4 +16,9 @@ export class TasksComponent implements OnInit {
     this.taskService.getTasks().subscribe((tasks) => this.tasks = tasks);
   }
 
+  deleteTask(task: Task) {
+    // delete task and when done filter out from UI
+    this.taskService.deleteTask(task).subscribe(() => this.tasks = this.tasks.filter(t => t.id !== task.id));
+  }
+
 }
